e83bc594444ed87bae71bd72b635b0f3d045257b
wanglei08 <wanglei08@ppdai.com>
2017-07-14 18:39:50 +0800

    创建工程

---
+++ FirstSpec/FirstSpec.h
@@ -0,0 +1,19 @@
+//
+//  FirstSpec.h
+//  FirstSpec
+//
+//  Created by Reino on 2017/7/14.
+//  Copyright © 2017年 Reino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FirstSpec.
+FOUNDATION_EXPORT double FirstSpecVersionNumber;
+
+//! Project version string for FirstSpec.
+FOUNDATION_EXPORT const unsigned char FirstSpecVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirstSpec/PublicHeader.h>
+
+
